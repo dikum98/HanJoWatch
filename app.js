6a206d67ef84e65d8902b698428db72b9ce00987
dikum98 <dikum98@gmail.com>
2022-08-17 20:33:59 +0900

    refactor: Encaptulate methods using IIFE
    - 프로토타입에서 생성자 함수 내부의 함수를 사용하기 위해 기존에 식별자에 할당되었던 함수들 (스코프가 달라서 접근할 수 없었음)을 this.constructor로 접근할 수 있는 프로퍼티로 바꿔줌.
    
    - 그러나 프로퍼티로 바꿔주었기 때문에 인스턴스를 생성할 때마다 재할당이 되어 처음 문제에 다시 봉착 (프로토타입 안쓰는 생성자 함수와 다를 바 없음)
    
    - 그래서 인스턴스에서는 직접 접근이 안되게 하면서도 프로토타입 메서드는 접근할 수 있고 인스턴스가 생성될 때 재할당되지 않도록 즉시 실행 함수를 활용하여 캡슐화 하였다.

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
 const select = (selector) => document.querySelector(selector);
 const timeElement = select('time');
 const controls = select('.controls');
-
-function HanjoWatch() {
-  this.timerId = null;
-  this.timeCount = 0;
-  this.constructor.INITIALTIME = '00:00:00.0';
-  this.constructor.setDigit = (item) => (item + '').padStart(2, '0');
-  this.constructor.parseTime = (timeCount) => {
-    let milliSeconds = this.timeCount % 10;
-    let seconds = parseInt(this.timeCount / 10);
-    seconds = this.constructor.setDigit(seconds > 59 ? seconds % 60 : seconds);
-    let minutes = this.timeCount > 599 ? parseInt(this.timeCount / 600) : 0;
-    minutes = this.constructor.setDigit(minutes);
+const HanjoWatch = (function () {
+  const INITIALTIME = '00:00:00.0';
+  const setDigit = (item) => (item + '').padStart(2, '0');
+  const parseTime = (timeCount) => {
+    let milliSeconds = timeCount % 10;
+    let seconds = parseInt(timeCount / 10);
+    seconds = setDigit(seconds > 59 ? seconds % 60 : seconds);
+    let minutes = timeCount > 599 ? parseInt(timeCount / 600) : 0;
+    minutes = setDigit(minutes);
     return `00:${minutes}:${seconds}:${milliSeconds}`;
   };
-}
-HanjoWatch.prototype.intervalHandler = function (timeElement) {
-  this.timeCount += 1;
-  timeElement.innerHTML = this.constructor.parseTime(this.timeCount);
-};
-HanjoWatch.prototype.runTimer = function (timeElement) {
-  this.timerId = setInterval(() => this.intervalHandler(timeElement), 100);
-};
-HanjoWatch.prototype.pauseTimer = function () {
-  clearInterval(this.timerId);
-};
-HanjoWatch.prototype.resumeTimer = function (timeElement) {
-  this.timerId = setInterval(() => this.intervalHandler(timeElement), 100);
-};
-HanjoWatch.prototype.resetTimer = function (timeElement) {
-  clearInterval(this.timerId);
-  this.timeCount = 0;
-  timeElement.innerHTML = this.constructor.INITIALTIME;
-};
+  function HanjoWatch() {
+    this.timerId = null;
+    this.timeCount = 0;
+  }
+  HanjoWatch.prototype.intervalHandler = function (timeElement) {
+    this.timeCount += 1;
+    timeElement.innerHTML = parseTime(this.timeCount);
+  };
+  HanjoWatch.prototype.runTimer = function (timeElement) {
+    this.timerId = setInterval(() => this.intervalHandler(timeElement), 100);
+  };
+  HanjoWatch.prototype.pauseTimer = function () {
+    clearInterval(this.timerId);
+  };
+  HanjoWatch.prototype.resumeTimer = function (timeElement) {
+    this.timerId = setInterval(() => this.intervalHandler(timeElement), 100);
+  };
+  HanjoWatch.prototype.resetTimer = function (timeElement) {
+    clearInterval(this.timerId);
+    this.timeCount = 0;
+    timeElement.innerHTML = INITIALTIME;
+  };
+  return HanjoWatch;
+})();
 
 const newHanjo = new HanjoWatch();
 
